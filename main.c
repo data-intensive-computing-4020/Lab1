26ac1f0662d9d3011ded8884d25d291af0d5b0a9
SoIidarity <christopher.maree@gmail.com>
2018-02-21 17:55:38 +0200

    Added Second Procedure

--- main.c
+++ main.c
@@ -1,49 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-/*  Note:  For demonstration purposes only.  Depending on needs, other types
-    might be used for indices and sizes, and the array type might be wrapped
-    in an opaque struct rather than exposed as "int *".
-*/
-
-
-//  Create an array with N dimensions with sizes specified in D.
 int *CreateArray(size_t N, size_t D[])
 {
-    //  Calculate size needed.
     size_t s = sizeof(int);
     for (size_t n = 0; n < N; ++n)
         s *= D[n];
 
-    //  Allocate space.
     return malloc(s);
 }
 
-/*  Return a pointer to an element in an N-dimensional A array with sizes
-    specified in D and indices to the particular element specified in I.
-*/
 int *Element(int *A, size_t N, size_t D[], size_t I[])
 {
-    //  Handle degenerate case.
     if (N == 0)
         return A;
 
-    //  Map N-dimensional indices to one dimension.
     int index = I[0];
     for (size_t n = 1; n < N; ++n)
         index = index * D[n] + I[n];
-
-    //  Return address of element.
+    
     return &A[index];
+}
+
+size_t numberOfElements(size_t N, size_t A[]){
+    size_t numberOfElements=1;
+    for (int i=0;i<N;i++){
+        numberOfElements *=A[i];
+    }
+    return numberOfElements;
+}
+
+void secondProcedure(int N, int *A, size_t D[]){
+    int _numberOfElements = numberOfElements(N,D);
+    int numberOfBlocksToChange = ceil(_numberOfElements/10);
+    printf("number of elements to change: %d \n",numberOfBlocksToChange);
+    int numberOfBlockesChanged = 0;
+    srand(time(NULL));   // generate random seed
+    while (numberOfBlockesChanged!=numberOfBlocksToChange){
+        int randomPos = rand() % _numberOfElements;
+        numberOfBlockesChanged++;
+        A[randomPos]=1;
+    }
 }
 
 int main()
 {
-    //  Create a 3*3*7*7*9 array.
-    size_t Size[5] = { 3, 3, 7, 7, 9 };
-    int *Array = CreateArray(5, Size);
+    size_t Size[4] = { 3, 3, 3, 5 };
+
+    size_t N = sizeof(Size)/ sizeof(Size[0]);
+
+
+    printf("Total Number of dimensions: %d \n Total number of elements %d \n",N,numberOfElements(N,Size));
+
+    int *Array = CreateArray(N, Size);
 
     //  Set element [1][2][3][4][5] to -987.
-    *Element(Array, 5, Size, (size_t []) { 1, 2, 3, 4, 5 }) = -987;
-    printf("element set: %d",*Element(Array, 5, Size, (size_t []) { 1, 2, 3, 4, 5 }));
+    *Element(Array, N, Size, (size_t []) { 1, 2, 3, 4, 5 }) = -987;
+    printf("element set: %d\n",*Element(Array, N, Size, (size_t []) { 1, 2, 3, 4, 5 }));
+
+    secondProcedure(N,Array,Size);
 }
